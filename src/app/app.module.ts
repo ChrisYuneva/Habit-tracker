1630fc69b911f7c78cec86d344d41688d8a7d984
ChrisYuneva <christina_yuneva@mail.ru>
2022-04-01 20:23:53 +0400

    Add scale for card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContainerComponent } from './container/container.component';
 import { CardComponent } from './card/card.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,11 @@ import { CardComponent } from './card/card.component';
     HeaderComponent,
     ContainerComponent,
     CardComponent,
+    UserComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  count: number = 0;
+  del: number = 0;
+
+  habit: string = 'Привычка';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  test(): void {
+    // this.count=true;
+    this.count++;
+    console.log(this.count);
+  }
+
+  minus(): void {
+    this.count--;
+    console.log(this.count);
+  }
+
+  delCard(): void {
+    this.del++;
   }
 
 }

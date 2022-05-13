c8368871518047e27ad3a65bfe5c34150a80f4f6
ChrisYuneva <christina_yuneva@mail.ru>
2022-05-13 17:08:40 +0400

    Added validation for forms

--- src/app/authorization/authorization.component.ts
+++ src/app/authorization/authorization.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+
+export class User {
+  constructor(public login: string,
+              public password: string) {
+  }
+}
 
 @Component({
   selector: 'app-authorization',
@@ -7,9 +14,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthorizationComponent implements OnInit {
 
-  constructor() { }
+  login: string = 'test';
+  password: string = 'Qazwsxedc_01022';
+  uncorrected: boolean = false;
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
   }
 
+  user: User = new User('', '')
+
+  addUser() {
+    console.log(this.user);
+    if (this.user.login === this.login && this.user.password === this.password) {
+      this.router.navigate(['/habits']);
+    } else {
+      this.uncorrected = true;
+    }
+  }
+
+  ngOnInit(): void {
+
+  }
 }

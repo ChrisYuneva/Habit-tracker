9c7878a6145d07712503503ee1a909d3486b55bf
ChrisYuneva <christina_yuneva@mail.ru>
2022-07-25 22:10:14 +0400

    fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<habit-tracker>'));
+app.use(express.static(__dirname + '/dist/habit-tracker'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/<habit-tracker>/index.html'));});
+    '/dist/habit-tracker/index.html'));});
 app.listen(process.env.PORT || 8080);
